5efac644e145f67ec36ac077c5b6e7971379bdd0
z <elena.samchuk.73@mail.ru>
2021-03-12 22:34:19 +0300

    MAIN

--- DQ.cpp
+++ DQ.cpp
@@ -15,28 +15,36 @@ int main(int argc, char* argv[])
 	D.push_front(3);   // D = 3 2 2 4
 	if (argc == 0 || argc == 1)exit(1);
 	if (string(argv[1]) == "push_front") {
-		if (argv[2] == NULL)exit(1);
+		if (argv[2] == NULL) {
+			cout << "Error";
+			exit(1);
+		}
 		int n;
 		n = atoi(string(argv[2]).c_str());
 		D.push_front(n);          // D = n 3 2 2 4
 		D.print();
 	}
 	if (string(argv[1]) == "push_back") {
+		if (argv[2] == NULL) {
+			cout << "Error";
+			exit(1);
+		}
 		if (argv[2] == NULL)exit(1);
 		int n;
 		n = atoi(string(argv[2]).c_str());
-		D.push_front(n);          // D = 3 2 2 4 n
+		D.push_back(n);          // D = 3 2 2 4 n
 		D.print();
 	}
 	if (string(argv[1]) == "pop_front") {	
 		D.pop_front();          // D = 2 2 4
 		D.print();
 	}
-	if (string(argv[1]) == "pop_front") {
-		D.pop_front();          // D = 2 2 4
+	if (string(argv[1]) == "pop_back") {
+		D.pop_back();          // D = 3 2 2 
 		D.print();
 	}
 	if (string(argv[1]) == "menu") {
+		D.clear();
 		int comand;
 		int x;
 		cout << "1 - push_front / 2- push_back / 3- pop_front / 4- pop_back / 5- size / 6 - clear / 7 - exit / 8 - print" << endl;
