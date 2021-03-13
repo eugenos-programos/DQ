ee67060d6bde2b42699db2591e7a5af5f5503523
eugenos-programos <71227355+eugenos-programos@users.noreply.github.com>
2021-03-13 10:58:28 +0300

    Update batchnik.bat

--- batchnik.bat
+++ batchnik.bat
@@ -4,31 +4,33 @@ touch test_push_back.txt
 touch test_push_front.txt
 touch test_pop_front.txt
 touch test_pop_back.txt
-@echo "Testing push front function is doing..."
-TIMEOUT /T 2 /NOBREAK
-C:\Users\ASUS\source\repos\DQ\Debug\DQ.exe testing_for_push_front > C:\tests\test_push_front.txt
+echo INITIAL DEQUE - 3 2 2 4
+echo push front 4
+C:\Users\ASUS\source\repos\DQ\Debug\DQ.exe push_front 4 > test_push_front.txt | type test_push_front.txt
 fc test_push_front.txt test_push_front_RES.txt
-if not ERRORLEVEL 1 echo PUSH FRONT FUNCTION TEST PASSED SUCCESSFULLY
-@echo.
-@echo "Testing push back function is doing..."
-TIMEOUT /T 2 /NOBREAK
-C:\Users\ASUS\source\repos\DQ\Debug\DQ.exe testing_for_push_back > C:\tests\test_push_back.txt
+if not ERRORLEVEL 1 echo PUSH FRONT FUNCTION TEST PASSED
+if ERRORLEVEL 1 echo FAIL
+echo.
+echo push_back 5
+C:\Users\ASUS\source\repos\DQ\Debug\DQ.exe push_back 5 > test_push_back.txt | type test_push_back.txt
 fc test_push_back.txt test_push_back_RES.txt
 if not ERRORLEVEL 1 echo PUSH BACK FUNCTION TEST PASSED SUCCESSFULLY
-@echo.
-@echo "Testing pop front function is doing..."
-TIMEOUT /T 2 /NOBREAK
-C:\Users\ASUS\source\repos\DQ\Debug\DQ.exe testing_for_pop_front > C:\tests\test_pop_front.txt
+if ERRORLEVEL 1 echo FAIL
+echo.
+echo pop_front 
+C:\Users\ASUS\source\repos\DQ\Debug\DQ.exe pop_front > test_pop_front.txt | type test_pop_front.txt
 fc test_pop_front.txt test_pop_front_RES.txt
 if not ERRORLEVEL 1 echo POP FRONT FUNCTION TEST PASSED SUCCESSFULLY
-@echo.
-@echo "Testing pop back function is doing..."
-TIMEOUT /T 2 /NOBREAK
-C:\Users\ASUS\source\repos\DQ\Debug\DQ.exe testing_for_pop_back > C:\tests\test_pop_back.txt
-fc test_push_back.txt test_push_back_RES.txt
+if ERRORLEVEL 1 echo FAIL
+echo.
+echo pop_back 
+C:\Users\ASUS\source\repos\DQ\Debug\DQ.exe pop_back > test_pop_back.txt | type test_pop_back.txt
+fc test_pop_back.txt test_pop_back_RES.txt
 if not ERRORLEVEL 1 echo POP BACK FUNCTION TEST PASSED SUCCESSFULLY
-@echo.
-C:\Users\ASUS\source\repos\DQ\Debug\DQ.exe menu
+if ERRORLEVEL 1 echo FAIL
+echo.
+pause
+
 
 
 
